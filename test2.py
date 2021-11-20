3bcf5555d812cf02f2b90d713fcd8cd83818d349
krishna kunal <calmkunal2003@gmail.com>
2021-11-20 19:18:33 +0530

    added function to square numbers

--- test2.py
+++ test2.py
@@ -9,3 +9,11 @@ def sum(num1, num2):
 
 total = sum(10, 20)
 print(total(10,20))
+
+
+def sqr_dict():
+     dict = {i:i**2 for i in range(1,21)}
+     return dict
+     return dict.keys() # print only keys
+
+print(sqr_dict())
